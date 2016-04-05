ea2f141c22491c199c3250fdc8dd45e613c4f172
Gadareth Higgs <gadareth.h@gmail.com>
2016-04-04 20:16:01 -0400

    added JS functionality

--- client/madLib.js
+++ client/madLib.js
@@ -1,10 +1,32 @@
 Template.body.events({
 
-	'submit form': function(event) {
+	/*'submit form': function(event) {
 
 		event.preventDefault();
 
-		console.log("Hello, world");
+		//console.log("Hello, world");
 
+
+		let exclamation = template.getElementById('#exclamation').value;
+		let adverb = template.getElementById('#adverb').value;
+		let animal = template.getElementById('#animal').value;
+		let verb = template.getElementById('#verb').value;
+
+		let madLib = exclamation "you shout as you"+ " " + adverb + "  " + animal + " " + verb;
+		console.log(madLib);
+	}
+});
+*/
+	'submit form'(event) {
+	event.preventDefault();
+
+	const target = event.target;
+	const exclamation = target.exclamation.value;
+	const adverb = target.adverb.value;
+	const animal = target.animal.value;
+	const verb = target.verb.value;
+
+	const madLib = `'${exclamation}' you say as you jump ${adverb} onto your ${animal} as you ${verb} into the sunset`;
+	console.log(madLib);
 	}
 });
